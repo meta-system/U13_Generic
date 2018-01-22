2022ffa57d14270b058f7ba08b9edd02d6e3cccd
Christian David Zdralek <christian.zdralek@student.uni-halle.de>
2018-01-22 14:57:44 +0100

    Slight advancement

--- Getraenkekasten.java
+++ Getraenkekasten.java
@@ -1,8 +1,28 @@
 public class Getraenkekasten <T>{
-	private T[] t;
-	private int anzahlFlaschen;
+	private T[] tArr;
+	private int anzahlFlaschen, pos;
 	
-	public Getraenkekasten (T[] t) {
-		this.t = t;
+	public Getraenkekasten (T[] tArr) {
+		this.tArr = t;
+		
+	}
+	
+	public boolean neueFlasche (T t) {
+		
+	}
+	
+	public String wovonAmMeisten (){
+		if (anzahlFlaschen+1 == 0){
+			return "";
+		}
+		String[] desc = new String[anzahlFlaschen+1];
+		int[] counter = new int [anzahlFlaschen+1];
+		
+		for (int i = 0; i < anzahlFlaschen+1; i++){
+			for (int j = 0; j < desc.length; j++){
+			
+			 	if(tArr[].toString().equals(
+			 }
+		}
 	}
 }
